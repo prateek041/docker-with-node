5d2bd31d6435e4bb4e2c494e03ff822c118a174b
prateek041 <prateeksingh9741@gmail.com>
2022-07-12 08:40:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h2>Hello there</h2>")
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
